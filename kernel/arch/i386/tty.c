b2a077db4c459ac55000a971db91cfea176df03d
chair1842 <207965457+chair1842@users.noreply.github.com>
2026-05-13 13:14:23 +0800

    add newline support and scrolling

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -41,10 +41,15 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 void terminal_putchar(char c) {
 	unsigned char uc = c;
 	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
+	if (c == '\n') {
+		terminal_row++;
+		terminal_column = 0; 
+	}
+
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
+			terminal_scroll();
 	}
 }
 
@@ -55,4 +60,18 @@ void terminal_write(const char* data, size_t size) {
 
 void terminal_writestring(const char* data) {
 	terminal_write(data, strlen(data));
+}
+
+void terminal_scroll(void) {
+	for (size_t y = 1; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t from_index = y * VGA_WIDTH + x;
+			const size_t to_index = (y - 1) * VGA_WIDTH + x;
+			terminal_buffer[to_index] = terminal_buffer[from_index];
+		}
+	}
+	for (size_t x = 0; x < VGA_WIDTH; x++) {
+		const size_t index = (VGA_HEIGHT - 1) * VGA_WIDTH + x;
+		terminal_buffer[index] = vga_entry(' ', terminal_color);
+	}
 }
