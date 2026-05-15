1911859121609119c3c9c17cabc48567b9935e2b
chair1842 <207965457+chair1842@users.noreply.github.com>
2026-05-15 10:04:19 +0800

    add qemu logging and disable shutdown in qemu script

--- qemu.sh
+++ qemu.sh
@@ -2,4 +2,4 @@
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom panuti.iso -no-reboot -serial stdio
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom panuti.iso -no-reboot -serial stdio -no-shutdown -d int -D qemu.log
