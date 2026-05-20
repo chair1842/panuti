94d509d9c937350d3c53f9bc66737836ad4b6bba
chair1842 <207965457+chair1842@users.noreply.github.com>
2026-05-20 19:32:50 +0800

    feat: implement physical memory manager initialization and multiboot info handling

---
+++ kernel/arch/i386/memman/pmm/pmm.c
@@ -0,0 +1,5 @@
+#include "pmm.h"
+
+void pmm_init(multiboot_info_t* mbi) {
+    // uhm
+}
---
+++ kernel/arch/i386/memman/pmm/pmm.h
@@ -0,0 +1,9 @@
+#ifndef ARCH_I386_MEMANPMM_H
+#define ARCH_I386_MEMANPMM_H
+
+#include <stdint.h>
+#include "../../multiboot.h"
+
+void pmm_init(multiboot_info_t* mbi);
+
+#endif
