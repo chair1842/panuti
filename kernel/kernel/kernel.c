7b107d8d79a7cedb8db60b1ab489f10455e62337
chair1842 <207965457+chair1842@users.noreply.github.com>
2026-07-07 17:31:12 +0800

    cheduling works

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,8 +1,29 @@
+#include "kernel/sched/task.h"
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/sched/sched.h>
+#include <kernel/klog.h>
+
+void a(void) {
+	while (1) {
+		klog(KLOG_INFO, "a\n");
+		sched_schedule();
+	}
+}
+
+void b(void) {
+	while (1) {
+		klog(KLOG_INFO, "b\n");
+		sched_schedule();
+	}
+}
 
 void kernel_main(void) {
 	terminal_initialize();
 	printf("Panuti Kernel\n");
+	
+	task_create(a);
+	task_create(b);	
+	sched_init();
 }
