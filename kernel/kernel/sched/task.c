8e7c9c679ff84e3aae29694b52c20b9f3b27c04b
chair1842 <207965457+chair1842@users.noreply.github.com>
2026-07-07 14:28:00 +0800

    Update task.c

--- kernel/kernel/sched/task.c
+++ kernel/kernel/sched/task.c
@@ -2,13 +2,11 @@
 #include <kernel/memman/vmalloc.h>
 #include <kernel/memman/memman.h>
 #include <stdint.h>
-#include <stddef.h>
 
 #define MAX_TASKS 64 
 
 static uint32_t next_pid = 1;
 static task_t tasks[MAX_TASKS] = {0};
-static task_t* current = NULL;
 
 task_t* task_create(void (*entry)(void)) {
 	task_t* t = &tasks[next_pid];
@@ -16,5 +14,8 @@ task_t* task_create(void (*entry)(void)) {
 	t->pid = next_pid++;
 	t->state = TASK_READY;
 	t->addr_space = memman_create_addr_space();
+	
+	task_init_stack(t, entry);
+	
 	return t;
 }
