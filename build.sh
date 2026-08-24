f2fc4ee6f896b8d113e3735943991ff14985ad58
chair1842 <207965457+chair1842@users.noreply.github.com>
2026-08-24 22:56:20 +0800

    bear

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 set -e
 . ./headers.sh
 
+if command -v bear >/dev/null 2>&1; then
+  MAKE="bear -- $MAKE"
+fi
+
 for PROJECT in $PROJECTS; do
   (cd $PROJECT && DESTDIR="$SYSROOT" $MAKE install)
 done
