e5b6157d933b5202a28a5d2b52ae2582ad3b45a1
chair1842 <207965457+chair1842@users.noreply.github.com>
2026-09-06 22:24:55 +0800

    add inw and outw

--- kernel/arch/i386/io.c
+++ kernel/arch/i386/io.c
@@ -10,6 +10,24 @@ uint8_t inb(uint16_t port) {
 	return ret;
 }
 
+void outw(uint16_t port, uint16_t value) {
+	__asm__ volatile ("outw %0, %1" : : "a"(value), "Nd"(port));
+}
+
+uint16_t inw(uint16_t port) {
+	uint16_t ret;
+	__asm__ volatile ("inw %1, %0" : "=a"(ret) : "Nd"(port));
+	return ret;
+}
+
+void outsw(uint16_t port, const void* buffer, size_t count) {
+	__asm__ volatile ("rep outsw" : "+S"(buffer), "+c"(count) : "d"(port) : "memory");
+}
+
+void insw(uint16_t port, void* buffer, size_t count) {
+	__asm__ volatile ("rep insw" : "+D"(buffer), "+c"(count) : "d"(port) : "memory");
+}
+
 void iowait(void) {
 	__asm__ volatile ("outb %%al, $0x80" : : "a"(0));
 }
--- kernel/arch/i386/io.h
+++ kernel/arch/i386/io.h
@@ -2,9 +2,14 @@
 #define ARCH_I386_IO_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 void outb(uint16_t port, uint8_t value);
 uint8_t inb(uint16_t port);
+void outw(uint16_t port, uint16_t value);
+uint16_t inw(uint16_t port);
+void outsw(uint16_t port, const void* buffer, size_t count);
+void insw(uint16_t port, void* buffer, size_t count);
 void iowait(void);
 
 #endif
