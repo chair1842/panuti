41af257edf4c17e8affb312e0f84998f869ba744
chair1842 <207965457+chair1842@users.noreply.github.com>
2026-09-11 16:14:46 +0800

    oops

--- kernel/kernel/syscall/syscall.c
+++ kernel/kernel/syscall/syscall.c
@@ -21,6 +21,7 @@ syscall_handler_t syscall_handlers[256] = {
 	[SYSHANDLER_LINK] = syshandler_link,
 	[SYSHANDLER_MOUNT] = syshandler_mount,
 	[SYSHANDLER_UNMOUNT] = syshandler_unmount,
+	[SYSHANDLER_PIPE_CREATE] = syshandler_pipe_create,
 };
 
 int32_t syscall_dispatch(uint32_t num, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4) {
